845583addb432e0982fbf05516d9e83e5e830536
hitesh.savaliya@gmail.com <hitesh.savaliya@gmail.com>
2012-12-30 13:22:13 +0000

    o URL simplified

--- src/holidayTracker/urls.py
+++ src/holidayTracker/urls.py
@@ -4,15 +4,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('holiTrack.views',
     # Examples:
-    url(r'^$', 'holiTrack.views.home', name='home'),
-    url(r'^employee/(?P<emp_id>\d+)/$', 'holiTrack.views.details', name='details'),
+    url(r'^employee/$', 'home', name='home'),
+    url(r'^employee/(?P<emp_id>\d+)/$', 'details', name='details'),
     # url(r'^holidayTracker/', include('holidayTracker.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns ('', url(r'^admin/', include(admin.site.urls)),)
