ecce41b59750bc68a23f2e1de12a3519da138720
hitesh.savaliya@gmail.com <hitesh.savaliya@gmail.com>
2012-12-30 13:35:26 +0000

    o URLConf simplified

---
+++ src/holiTrack/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('holiTrack.views',
+    # Examples:
+    url(r'^$', 'home', name='home'),
+    url(r'^(?P<emp_id>\d+)/$', 'details', name='details'),
+    # url(r'^holidayTracker/', include('holidayTracker.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+)
+
+urlpatterns += patterns ('', url(r'^admin/', include(admin.site.urls)),)
--- src/holidayTracker/urls.py
+++ src/holidayTracker/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
+
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('holiTrack.views',
-    # Examples:
-    url(r'^employee/$', 'home', name='home'),
-    url(r'^employee/(?P<emp_id>\d+)/$', 'details', name='details'),
-    # url(r'^holidayTracker/', include('holidayTracker.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-)
-
-urlpatterns += patterns ('', url(r'^admin/', include(admin.site.urls)),)
+urlpatterns = patterns('',
+    url(r'^employee/', include('holiTrack.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    )
